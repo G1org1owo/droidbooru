0e43760ffadc301fbd9925301b50f2b3eb08b317
G1org1owo <77230371+G1org1owo@users.noreply.github.com>
2024-08-21 14:54:35 +0200

    Refactor method names and extract offset recalculations

--- lib/ui/post_interactive_image.dart
+++ lib/ui/post_interactive_image.dart
@@ -58,18 +58,20 @@ class _PostInteractiveImageState extends State<PostInteractiveImage> {
     );
   }
 
-  void _onScaleStart(details) {
+  void _onScaleStart(ScaleStartDetails details) {
       _previousScale = _matrix.getMaxScaleOnAxis();
       if(details.pointerCount == 2) {
-        _origin = calculateOrigin(_previousScale, details.focalPoint);
+        _origin = _calculateOrigin(_previousScale, details.focalPoint);
       }
 
       widget._onScaleStart?.call(details);
-    }
-
-  void _onScaleUpdate(details) {
+  }
+  void _onScaleUpdate(ScaleUpdateDetails details) {
       if(details.pointerCount != 2) return;
 
+      /* TODO: if widget._maxScale is 0, allow infinite zooming or up to 1.5x
+               the screen size relative to the smallest dimension of the image
+      */
       double newScale = _previousScale * details.scale;
       if(newScale > widget._maxScale) {
         newScale = widget._maxScale;
@@ -77,31 +79,30 @@ class _PostInteractiveImageState extends State<PostInteractiveImage> {
         newScale = widget._minScale;
       }
 
-      applyScale(newScale, _origin!);
+      _applyScale(newScale, _origin!);
       widget._onScaleUpdate?.call(details);
-    }
-
-  void _onScaleEnd(details) {
+  }
+  void _onScaleEnd(ScaleEndDetails details) {
       _previousScale = 0;
       if(details.pointerCount == 0) {
         _origin = null;
       }
       widget._onScaleEnd?.call(details);
-    }
+  }
 
-  void _onDoubleTapDown(details) {
+  void _onDoubleTapDown(TapDownDetails details) {
       double scale = _matrix.getMaxScaleOnAxis() % widget._maxScale + widget._minScale;
 
       // TODO: add zoom-in animation for double tap
-      applyScale(
+      _applyScale(
         scale,
-        calculateOrigin(scale, details.localPosition),
+        _calculateOrigin(scale, details.localPosition),
       );
-    }
+  }
 
-  Offset calculateOrigin(double scale, Offset focalPoint) {
+  Offset _calculateOrigin(double scale, Offset focalPoint) {
     Size screenSize = MediaQuery.sizeOf(context);
-    Size renderedSize = getRenderedSize(
+    Size renderedSize = _getRenderedSize(
         screenSize,
         Size(post.width.toDouble(), post.height.toDouble())
     );
@@ -114,8 +115,7 @@ class _PostInteractiveImageState extends State<PostInteractiveImage> {
 
     return Offset(dx, dy);
   }
-
-  Size getRenderedSize(Size containerSize, Size imageSize) {
+  Size _getRenderedSize(Size containerSize, Size imageSize) {
     double renderedWidth = 0;
     double renderedHeight = 0;
 
@@ -129,10 +129,34 @@ class _PostInteractiveImageState extends State<PostInteractiveImage> {
 
     return Size(renderedWidth, renderedHeight);
   }
+  Matrix4 _recalculateOffset(double scale, Matrix4 matrix, Size screenSize, Size renderedSize) {
+    Matrix4 newMatrix = matrix.clone();
 
-  void applyScale(double newScale, Offset origin) async {
+    // If the previous calculations end up not being centered, fix the offset
+    // to avoid zooming towards the black bars (this is the only reason
+    // I had to rewrite InteractiveViewer from scratch)
+    if (renderedSize.width/2 * scale + newMatrix.row0.w <= renderedSize.width/2) {
+      newMatrix.setEntry(0, 3, (renderedSize.width - renderedSize.width * scale) / 2);
+    } else if (-renderedSize.width/2 * scale + newMatrix.row0.w >= -renderedSize.width/2) {
+      newMatrix.setEntry(0, 3, (renderedSize.width * scale - renderedSize.width) / 2);
+    } else if (renderedSize.width/2 * scale + newMatrix.row0.w <= screenSize.width/2) {
+      newMatrix.setEntry(0, 3, 0.0);
+    }
+
+    if (renderedSize.height/2 * scale + newMatrix.row1.w <= renderedSize.height/2) {
+      newMatrix.setEntry(1, 3, (renderedSize.height - renderedSize.height * scale) / 2);
+    } else if (-renderedSize.height/2 * scale + newMatrix.row1.w >= -renderedSize.height/2) {
+      newMatrix.setEntry(1, 3, (renderedSize.height * scale - renderedSize.height) / 2);
+    } else if (renderedSize.height/2 * scale + newMatrix.row1.w <= screenSize.height/2) {
+      newMatrix.setEntry(1, 3, 0.0);
+    }
+
+    return newMatrix;
+  }
+
+  void _applyScale(double newScale, Offset origin) async {
     Size screenSize = MediaQuery.sizeOf(context);
-    Size renderedSize = getRenderedSize(
+    Size renderedSize = _getRenderedSize(
         screenSize,
         Size(post.width.toDouble(), post.height.toDouble())
     );
@@ -160,24 +184,7 @@ class _PostInteractiveImageState extends State<PostInteractiveImage> {
         origin.dx * newScale/previousScale - origin.dx, 0, 0, 1
       ));
 
-    // If the previous calculations end up not being centered, fix the offset
-    // to avoid zooming towards the black bars (this is the only reason
-    // I had to rewrite InteractiveViewer from scratch)
-    if (renderedSize.width/2 * newScale + newMatrix.row0.w <= renderedSize.width/2) {
-      newMatrix.setEntry(0, 3, (renderedSize.width - renderedSize.width * newScale) / 2);
-    } else if (-renderedSize.width/2 * newScale + newMatrix.row0.w >= -renderedSize.width/2) {
-      newMatrix.setEntry(0, 3, (renderedSize.width * newScale - renderedSize.width) / 2);
-    } else if (renderedSize.width/2 * newScale + newMatrix.row0.w <= screenSize.width/2) {
-      newMatrix.setEntry(0, 3, 0.0);
-    }
-
-    if (renderedSize.height/2 * newScale + newMatrix.row1.w <= renderedSize.height/2) {
-      newMatrix.setEntry(1, 3, (renderedSize.height - renderedSize.height * newScale) / 2);
-    } else if (-renderedSize.height/2 * newScale + newMatrix.row1.w >= -renderedSize.height/2) {
-      newMatrix.setEntry(1, 3, (renderedSize.height * newScale - renderedSize.height) / 2);
-    } else if (renderedSize.height/2 * newScale + newMatrix.row1.w <= screenSize.height/2) {
-      newMatrix.setEntry(1, 3, 0.0);
-    }
+    newMatrix = _recalculateOffset(newScale, newMatrix, screenSize, renderedSize);
 
     setState(() {
       _matrix = newMatrix;
